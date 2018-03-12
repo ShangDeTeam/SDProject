a0d0081e0b9584c08567446afaceb46bbde2dc22
BestPeter <3116174425@qq.com>
2018-03-12 16:03:49 +0800

    添加supervisor，服务器配置

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-var express = require('express');
+var http = require('http');
 var path = require('path');
+var express = require('express');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -12,8 +13,8 @@ var app = express();
 
 //设置模板引擎,目录为views
 nunjucks.configure(path.join(__dirname, 'views'), {
-  autoescape: true,
-  express: app
+	autoescape: true,
+	express: app
 });
 
 // view engine setup
@@ -23,17 +24,19 @@ app.set('view engine', 'html');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
 app.use(cookieParser());
 app.use(express.static('public'));
 
 app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handlers
@@ -41,24 +44,91 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+	app.use(function (err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
 });
 
 
-module.exports = app;
+/**
+ * Create HTTP server.
+ */
+var debug = require('debug')('SDProject:server');
+var port = normalizePort(process.env.PORT || '3000');
+var server = http.createServer(app);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+	var port = parseInt(val, 10);
+  
+	if (isNaN(port)) {
+	  // named pipe
+	  return val;
+	}
+  
+	if (port >= 0) {
+	  // port number
+	  return port;
+	}
+  
+	return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+function onError(error) {
+	if (error.syscall !== 'listen') {
+		throw error;
+	}
+
+	var bind = typeof port === 'string' ?
+		'Pipe ' + port :
+		'Port ' + port;
+
+	// handle specific listen errors with friendly messages
+	switch (error.code) {
+		case 'EACCES':
+			console.error(bind + ' requires elevated privileges');
+			process.exit(1);
+			break;
+		case 'EADDRINUSE':
+			console.error(bind + ' is already in use');
+			process.exit(1);
+			break;
+		default:
+			throw error;
+	}
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+function onListening() {
+	var addr = server.address();
+	var bind = typeof addr === 'string'
+	  ? 'pipe ' + addr
+	  : 'port ' + addr.port;
+	debug('Listening on ' + bind);
+}
